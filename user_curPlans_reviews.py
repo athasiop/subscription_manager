c534f47b6421c84fc7c6edc9d2909ae97c18b822
dpapargy <57500536+dpapargy@users.noreply.github.com>
2020-12-27 14:56:54 +0200

    Added cancel button

--- user_curPlans_reviews.py
+++ user_curPlans_reviews.py
@@ -120,8 +120,77 @@ def updateReview(serviceName, textUpdateReview, updateReviewRating, updateReview
             j = j + 1
         updateReviewWindow.destroy()
     
-    
-
+def checkCancel():
+    serviceName = ""
+    curReview = treePlans.focus()
+    selectedRow = treePlans.item(curReview)
+    serviceName = str(selectedRow).split(", ")[0]
+    serviceName = serviceName[10: -1] 
+    if not serviceName == "":
+        cancelPlan(serviceName)
+    else:
+        tk.messagebox.showerror(title = "Select service", message = "Please select a service to cancel")
+  
+def cancelPlan(serviceName):
+    curReview = treePlans.focus()
+    selectedRow = treePlans.item(curReview)
+    bundleID = str(selectedRow).split(", ")[5]
+    bundleID = bundleID[0 : -1]
+    planType = str(selectedRow).split(", ")[3]
+    planType = planType[1:]
+    print(bundleID)
+    if not bundleID == "' '":
+        tk.messagebox.showinfo(title = "Canceling from bundle", message = "You cannot cancel a plan that is included in a purchased bundle")
+    else:
+        if tk.messagebox.askquestion(title = "Canceling plan", message = "Are you sure you want to cancel " + serviceName) == "yes":
+            sqlPlanID = "Select user_buys_subscription_plan.plan_id from " + \
+            "user_buys_subscription_plan join subscription_plan " +\
+            "on user_buys_subscription_plan.plan_id = subscription_plan.plan_id and " +\
+            "user_buys_subscription_plan.service_name = subscription_plan.service_name " +\
+            "where user_id = " + str(currUserID) + " and user_buys_subscription_plan.service_name = '" + serviceName + "'"
+            cur.execute(sqlPlanID)
+            planID = str(cur.fetchall()).replace('(', '').replace(')', '').replace(",",'')
+            sqlCancePlan = "Delete from user_buys_subscription_plan where service_name = '" + serviceName + "'" " and plan_id = '" + planID + "'"
+            cur.execute(sqlCancePlan)
+            treePlans.delete(*treePlans.get_children())
+            sqlPlan = "SELECT subscription_plan.service_name, plan_price, plan_type, plan_purchase_date from " +\
+            "user_buys_subscription_plan join subscription_plan " +\
+            "on user_buys_subscription_plan.plan_id = subscription_plan.plan_id and " +\
+            "user_buys_subscription_plan.service_name = subscription_plan.service_name " +\
+            "where user_id = " + str(currUserID)
+            
+            cur.execute(sqlPlan)
+            resSqlPlan = cur.fetchall()
+            
+            i = 1
+            for x in resSqlPlan:
+                strX = str(x)
+                strX = strX.replace('(', '').replace(')', '').replace("'",'').replace('datetime.datetime', '')
+                strX = strX.split(",")
+                treePlans.insert("", i, text = strX[0], values = (strX[1] + " \u20ac", strX[2], str(datetime.date(int(strX[3]), int(strX[4]), int(strX[5]))), " "))
+                i = i + 1
+            
+            sqlBundle = "SELECT bundle_contains_subscription_plan.bundle_id, bundle_contains_subscription_plan.service_name, plan_price, plan_type, bundle_purchase_date from " + \
+            "user_buys_bundle join bundle "+ \
+            "on user_buys_bundle.bundle_id = bundle.bundle_id " + \
+            "join bundle_contains_subscription_plan " + \
+            "on bundle.bundle_id = bundle_contains_subscription_plan.bundle_id " + \
+            "join subscription_plan "  + \
+            "on bundle_contains_subscription_plan.plan_id = subscription_plan.plan_id and " + \
+            "bundle_contains_subscription_plan.service_name = subscription_plan.service_name " + \
+            "where user_id = " + str(currUserID)
+            
+            
+            cur.execute(sqlBundle)
+            resSqlBundle = cur.fetchall()
+            
+            for x in resSqlBundle:
+                strX = str(x)
+                strX = strX.replace('(', '').replace(')', '').replace("'",'').replace('datetime.datetime', '')
+                strX = strX.split(",")
+                treePlans.insert("", i, text = (strX[1])[1:], values = (strX[2] + " \u20ac", strX[3], str(datetime.date(int(strX[4]), int(strX[5]), int(strX[6]))), strX[0])) # "\u2713"
+                i = i + 1
+        
 def checkReview():
     serviceName = ""
     curReview = treePlans.focus()
@@ -158,7 +227,6 @@ treePlans.heading("two", text = "Type",anchor = tk.W)
 treePlans.heading("three", text = "Purchase Date",anchor = tk.W)
 treePlans.heading("four", text = "Bundle",anchor = tk.W)
 
-
 treeReviews = ttk.Treeview(userWindow)
 treeReviews["columns"] = ("one","two")
 treeReviews.column("#0", width = 150, minwidth = 150, stretch = tk.NO)
@@ -173,11 +241,12 @@ treeReviews.heading("two", text = "Review Date",anchor = tk.W)
 
 lblActPlans = tk.Label(userWindow,text = "Active Subscriptions")  
 btnReviewPlan = tk.Button(userWindow, text = "Review Service", 
-                           command= lambda: checkReview())
+                           command = lambda: checkReview())
 
 lblReviews = tk.Label(userWindow, text = "Reviews (double click to see comment or update review)")
 
 lblActUser = tk.Label(userWindow, text = "")
+btnCancelPlan = tk.Button(userWindow, text = "Cancel Plan", command = lambda: checkCancel())
 
 
 sqlUser = "SELECT user_name, email from user where user_id = " + str(currUserID)
@@ -251,7 +320,9 @@ lblActUser.config(text = currUser)
 
 lblActUser.pack()
 lblActPlans.pack()
+
 treePlans.pack()
+btnCancelPlan.pack()
 
 btnReviewPlan.pack()
 lblReviews.pack()
